e80336aaa7ad21570d072ac8e465cb40c1ec97f6
Thomas <43654548+thethomaswgeorge@users.noreply.github.com>
2020-02-08 11:49:39 -0600

    Added Input Alert
    Added input alert in case user didn't fill in all fields

--- js/script.js
+++ js/script.js
@@ -18,30 +18,35 @@ function calculateBMI() {
     var htIn = parseFloat(document.getElementById('inches').value);
 
     var wt   = document.getElementById('weight').value;
-    var kg   = wt * .45;
-    var totalwt= (htFt * 12) + htIn;
-    var meter= totalwt * 0.025;
-    console.log("Meters: "+meter);
-    console.log("Height inches: " +totalwt);
-    var meter = meter *meter;
-    var bmi   = (kg/meter).toFixed(1);
-    $("#yourResults").removeClass("hidden");
-    document.getElementById("yourFinalResults").innerText = bmi+'%';
 
-    if((bmi < 18.5) && (bmi > 0)) {
-        document.getElementById("customMessage").innerText = 'You are currently underweight';
-    }else if((bmi >= 18.5) && (bmi <= 24.9)) {
-        //Normal Weight
-        document.getElementById("customMessage").innerText = 'You are currently normal weight';
-    }else if((bmi >= 25) && (bmi <= 29.9)){
-        //Overweight
-        document.getElementById("customMessage").innerText = 'You are currently Overweight';
-    }else if((bmi >= 30)) {
-        document.getElementById("customMessage").innerText = 'You are currently Obese';
-        //Obese
-    }else {
-        document.getElementById("customMessage").innerHTML = 'You are currently <i class="fas fa-skull-crossbones"></i>';
-        //Dead
+    if((htFt != "") && (htIn != "") && (wt != "")) {
+        var kg   = wt * .45;
+        var totalwt= (htFt * 12) + htIn;
+        var meter= totalwt * 0.025;
+        console.log("Meters: "+meter);
+        console.log("Height inches: " +totalwt);
+        var meter = meter *meter;
+        var bmi   = (kg/meter).toFixed(1);
+        $("#yourResults").removeClass("hidden");
+        document.getElementById("yourFinalResults").innerText = bmi+'%';
+    
+        if((bmi < 18.5) && (bmi > 0)) {
+            document.getElementById("customMessage").innerText = 'You are currently underweight';
+        }else if((bmi >= 18.5) && (bmi <= 24.9)) {
+            //Normal Weight
+            document.getElementById("customMessage").innerText = 'You are currently normal weight';
+        }else if((bmi >= 25) && (bmi <= 29.9)){
+            //Overweight
+            document.getElementById("customMessage").innerText = 'You are currently Overweight';
+        }else if((bmi >= 30)) {
+            document.getElementById("customMessage").innerText = 'You are currently Obese';
+            //Obese
+        }else {
+            document.getElementById("customMessage").innerHTML = 'You are currently <i class="fas fa-skull-crossbones"></i>';
+            //Dead
+        }
+    } else {
+        alert("Please complete all the inputs");
     }
 }
 function calculateRetirement() {
@@ -51,31 +56,36 @@ function calculateRetirement() {
     var savingsG   = document.getElementById('savingsGoal').value;
     var yearlyUserSaved;
     var startingSaved = 0;
-    if(percentS < '1') {
+    if((currentAge != "") && (annualSal != "") && (percentS != "") && (savingsG != "")) {
 
-    }else {
-        percentS = percentS / 100;
-    }
+        if(percentS < '1') {
 
-    do{
-        yearlyUserSaved = annualSal * percentS;
-        yearlyCompSaved = yearlyUserSaved * .35;
-        startingSaved = startingSaved + yearlyUserSaved + yearlyCompSaved;
-        $('#retirementTable tr:last').after('<tr><td>'+currentAge+'</td><td>$'+yearlyUserSaved+'</td><td>$'+yearlyCompSaved+'</td><td>$'+startingSaved+'</td></tr>');
-        console.log('Year: '+currentAge+' Currently Saved: $'+startingSaved+'  Savings goal: $'+savingsG);
-        if(startingSaved > savingsG) {
-            $("#displayRetireResults").append("<b>You will achieve your savings goal at age "+currentAge+" with a total of $"+startingSaved+" saved!</b>");
-            
-        } else {
-            currentAge++;
+        }else {
+            percentS = percentS / 100;
         }
-    }while((startingSaved <= savingsG)&& (currentAge != 101));
-    if((startingSaved < savingsG) && (currentAge == '101')) {
-        $("#displayDeath").removeClass("hidden");
-        console.log("You are dead");
-    }else {
-        $("#displayRetireResults").removeClass("hidden");
-        alert("You will achieve your savings goal at age "+currentAge+" with a total of $"+startingSaved+" saved!");
-        
+    
+        do{
+            yearlyUserSaved = annualSal * percentS;
+            yearlyCompSaved = yearlyUserSaved * .35;
+            startingSaved = startingSaved + yearlyUserSaved + yearlyCompSaved;
+            $('#retirementTable tr:last').after('<tr><td>'+currentAge+'</td><td>$'+yearlyUserSaved+'</td><td>$'+yearlyCompSaved+'</td><td>$'+startingSaved+'</td></tr>');
+            console.log('Year: '+currentAge+' Currently Saved: $'+startingSaved+'  Savings goal: $'+savingsG);
+            if(startingSaved > savingsG) {
+                $("#displayRetireResults").append("<b>You will achieve your savings goal at age "+currentAge+" with a total of $"+startingSaved+" saved!</b>");
+                
+            } else {
+                currentAge++;
+            }
+        }while((startingSaved <= savingsG)&& (currentAge != 101));
+        if((startingSaved < savingsG) && (currentAge == '101')) {
+            $("#displayDeath").removeClass("hidden");
+            console.log("You are dead");
+        }else {
+            $("#displayRetireResults").removeClass("hidden");
+            alert("You will achieve your savings goal at age "+currentAge+" with a total of $"+startingSaved+" saved!");
+            
+        }
+    } else {
+        alert("Please complete all the inputs");
     }
 }
