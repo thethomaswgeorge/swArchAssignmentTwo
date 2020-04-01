ab275e150cdfad5431a7cff0351da0f2e68d2f63
Thomas <43654548+thethomaswgeorge@users.noreply.github.com>
2020-04-01 15:20:59 -0500

    Updated Script Test

--- script.test.js
+++ script.test.js
@@ -1,6 +1,11 @@
 const functions = require('./script');
 
 test("Checks BMI for someone 5'8'' and 150 lbs.  Should return: normal", () => {
-    console.log(functions);
     expect(functions.calcBMI(5,8,150)).toBe("normal");
+});
+test("Checks BMI for someone 5'8'' and 300 lbs.  Should return: obese", () => {
+    expect(functions.calcBMI(5,8,300)).toBe("obese");
+});
+test("Checks BMI for if someone entered nothing.  Should return: error", () => {
+    expect(functions.calcBMI('','','')).toBe("error");
 });
