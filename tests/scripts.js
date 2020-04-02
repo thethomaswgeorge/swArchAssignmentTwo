5fd4c63a00dc073372ba1f6e9c639e1b9846ba27
Thomas <43654548+thethomaswgeorge@users.noreply.github.com>
2020-04-02 10:56:03 -0500

    Added NightWatch Test

--- tests/scripts.js
+++ tests/scripts.js
@@ -1,5 +1,5 @@
 module.exports = {
     'First BMI Test Case'(browser) {
-        browser.url('https://sw-qa-3.herokuapp.com/').waitForElementVisible('.nav-item').assert.containsText("#retireApp","Retirement Calc");
+        browser.url('https://sw-qa-3.herokuapp.com/').waitForElementVisible('.nav-item').setValue("#height","5").setValue("#inches","8").setValue("#weight","150").click("button#submitBMIBtn").assert.containsText("#yourResults","Your Results: 23.4%");
     }
 }
