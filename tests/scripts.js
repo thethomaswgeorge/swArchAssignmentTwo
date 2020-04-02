377600677972bcde3fe7d7cfe857dd76812dd587
Thomas <43654548+thethomaswgeorge@users.noreply.github.com>
2020-04-02 11:12:42 -0500

    Added IDs to main page and Updated Nightwatch Tests

--- tests/scripts.js
+++ tests/scripts.js
@@ -1,5 +1,11 @@
 module.exports = {
-    'First BMI Test Case'(browser) {
-        browser.url('https://sw-qa-3.herokuapp.com/').waitForElementVisible('.nav-item').setValue("#height","5").setValue("#inches","8").setValue("#weight","150").click("button#submitBMIBtn").assert.containsText("#yourResults","Your Results: 23.4%");
+    'BMI Test Cases: '(browser) {
+        browser.url('https://sw-qa-3.herokuapp.com/').waitForElementVisible('.nav-item').click("li#bmiApp").setValue("#height","5").setValue("#inches","8").setValue("#weight","150").click("button#submitBMIBtn").assert.containsText("#yourResults","Your Results: 23.4%");
+    },
+    'Retirement Calc Test Cases: '(browser) {
+        browser.url('https://sw-qa-3.herokuapp.com/').waitForElementVisible('.nav-item').click("li#retireApp").setValue("#currentAge","25").setValue("#annualSalary","10").setValue("#percentSaved",".5").setValue("savingsGoal","20").click("button#submitRetireBtn").dismissAlert().assert.containsText("#retireBresults","You will achieve your savings goal at age 27 with a total of $20.25 saved!");
+    },
+    'Testing Exit: '(browser) {
+        browser.url('https://sw-qa-3.herokuapp.com/').waitForElementVisible('.nav-item').click("li#exitApp").assert.cssProperty("#darkCover","display","block");
     }
 }
